71eb8b0a6e4c82b50bc485b46985af8fe44c78c2
Malcolm St. John <malcolm19@hotmail.com>
2021-10-27 11:59:17 -0400

    Exposed timestamps in json

--- pkg/data/conversation.go
+++ pkg/data/conversation.go
@@ -12,11 +12,11 @@ var ErrorGameNotFound = fmt.Errorf("game not found")
 
 // Conversation defines the structure for an API conversation.
 type Conversation struct {
-	ID        string    `json:"id" bson:"_id"`
-	UserID    []string  `json:"user_id" validate:"required"`
-	GameID    string    `json:"game_id"`
-	CreatedOn string    `json:"-"`
-	UpdatedOn string    `json:"-"`
+	ID        string   `json:"id" bson:"_id"`
+	UserID    []string `json:"user_id" validate:"required"`
+	GameID    string   `json:"game_id"`
+	CreatedOn string   `json:"created_on"`
+	UpdatedOn string   `json:"updated_on"`
 }
 
 // Conversations is a collection of Conversation
