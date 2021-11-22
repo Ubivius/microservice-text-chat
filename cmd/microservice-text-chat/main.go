fe2d02f7573b52bf1661c804b11029ca5fb2334b
Malcolm St. John <malcolm19@hotmail.com>
2021-11-21 19:03:29 -0500

    back to mongodb in main.go

--- cmd/microservice-text-chat/main.go
+++ cmd/microservice-text-chat/main.go
@@ -33,7 +33,7 @@ func main() {
 	metrics.StartPrometheusExporterWithName("text-chat")
 
 	// Database init
-	db := database.NewMockTextChat()
+	db := database.NewMongoTextChat()
 
 	// Creating handlers
 	textChatHandler := handlers.NewTextChatHandler(db)
